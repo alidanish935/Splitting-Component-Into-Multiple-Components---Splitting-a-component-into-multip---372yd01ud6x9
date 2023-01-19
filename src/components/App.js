66332890c2cb3b2d078b259e0482d12b18ee2723
Danish <alidanish936@gmail.com>
2023-01-20 00:01:47 +0530

    changed id

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,9 @@ import Footer from "./Footer";
 function App() {
     return (
         <div className="container">
-            <Header id="header" content="This is the header"/>
-            <Main id="main" content="This is the main"/>
-            <Footer id="footer" content="This is the footer"/>
+            <Header id="my-header" content="This is the header"/>
+            <Main id="my-main" content="This is the main"/>
+            <Footer id="my-footer" content="This is the footer"/>
         </div>
     );
 }
